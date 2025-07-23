210c83512e920dcf65df478113df210b5f4c6522
abhijeetkumar097 <aabhijeet097@gmail.com>
2025-07-23 14:16:57 +0530

    project delete

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -26,6 +26,23 @@ const Dashboard = () => {
   const handleClickTask = (id) => {
     navigate(`/task/${id}`)
   }
+
+  const handleDeleteProject = (id) => {
+  if (window.confirm("Are you sure you want to delete this project?")) {
+    axios.delete(`${import.meta.env.VITE_URL}/api/projects/${id}/delete`, {
+      headers: { Authorization: `Bearer ${token1}` }
+    })
+    .then(res => {
+      toast.success("Project deleted successfully!");
+      fetchDashboardData();
+    })
+    .catch(err => {
+      toast.error("Error deleting project.");
+      console.error("Delete project error:", err.response?.data || err.message);
+    });
+  }
+};
+
   return (
     <div className="p-1  m-20 mt-4">
       <h1 className="text-3xl font-semibold mb-4">Dashboard</h1>
@@ -35,11 +52,26 @@ const Dashboard = () => {
         {data.projects.length ? (
           <ul className='flex flex-wrap gap-10'>
             {data.projects.slice(0, 5).map(p => (
-              <li key={p.id} onClick={() => handleClick(p.id)} className="cursor-pointer ">
+              <li key={p.id}  className="cursor-pointer ">
                 <div className="p-2 ">
                   <div className="bg-gray-300 rounded-lg shadow">
-                  <div className="bg-gray-800 text-white text-center py-2 rounded-t">{p.project_name.toUpperCase()}</div>
-                  <div className="p-2 space-y-2 text-black">
+                  <div className="bg-gray-800 text-white py-2 rounded-t flex justify-between p-3">
+                    <div onClick={() => handleClick(p.id)}>
+                      {p.project_name.toUpperCase()}
+                    </div>
+                    <div>
+                        <button
+                        onClick={() => handleDeleteProject(p.id)}
+                        title='delete'
+                        className='mt-1'
+                      >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                          <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+                        </svg>
+                    </button>
+                    </div>
+                  </div>
+                  <div className="p-2 space-y-2 text-black" onClick={() => handleClick(p.id)}>
                   <div className='text-blue-500'>Description: <span className='text-black'>{p.description}</span></div>
                   <div className='text-blue-500'>Status: <span className='text-black'>{p.status}</span></div>
                   
